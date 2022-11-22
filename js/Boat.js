141d7f3da9fcb8bcce81b1db55012018af9f18b1
Aadvik09 <109258620+Aadvik09@users.noreply.github.com>
2022-11-21 18:28:33 -0800

    Add files via upload

---
+++ js/Boat.js
@@ -0,0 +1,33 @@
+class Boat {
+  constructor(x, y, width, height, boatPos) {
+  
+    this.body = Bodies.rectangle(x, y, width, height);
+    this.width = width;
+    this.height = height;
+
+    this.image = loadImage("./assets/boat.png");
+    this.boatPosition = boatPos;
+    World.add(world, this.body);
+  }
+
+  display() {
+    var angle = this.body.angle;
+    var pos = this.body.position;
+
+    push();
+    translate(pos.x, pos.y);
+    rotate(angle);
+    imageMode(CENTER);
+    image(this.image, 0, this.boatPosition, this.width, this.height);
+    pop();
+
+  
+  }
+
+  remove (index) {
+    setTimeout(()=>{
+      Matter.World.remove(world,boats[index].body);
+      delete boats[index];
+    },2000) 
+  }
+}
